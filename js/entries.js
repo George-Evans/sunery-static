1222eff87ba122e2744d160357d4ff3a5b06f37e
George Evans <george.r.evans@googlemail.com>
2018-08-10 17:07:15 +0200

    varying entry data

--- js/entries.js
+++ js/entries.js
@@ -27,97 +27,97 @@ var entries = [
 	{
 		source: "Kitchen Lights",
 		timestamp: new Date("2018-07-01T23:11"),
-		units: 12
+		units: 04
 	},
 	{
 		source: "Hammer Drill",
 		timestamp: new Date("2018-04-12T10:29"),
-		units: 11
+		units: 09
 	},
 	{
 		source: "Air Conditioning",
 		timestamp: new Date("2018-05-16T13:32"),
-		units: 09
+		units: 11
 	},
 	{
 		source: "Washing Machine",
-		timestamp: new Date("2018-05-12T17:00"),
-		units: 21
-	},
-	{
-		source: "Heating",
-		timestamp: new Date("2018-06-22T21:10"),
-		units: 19
-	},
-	{
-		source: "TV",
-		timestamp: new Date("2018-06-08T20:09"),
-		units: 18
-	},
-	{
-		source: "Hairdryer",
-		timestamp: new Date("2018-05-09T07:18"),
-		units: 14
-	},
-	{
-		source: "Tumble Dryer",
-		timestamp: new Date("2018-07-30T11:47"),
-		units: 13
-	},
-	{
-		source: "Kitchen Lights",
-		timestamp: new Date("2018-07-01T23:11"),
+		timestamp: new Date("2018-07-12T11:30"),
 		units: 12
 	},
 	{
+		source: "Heating",
+		timestamp: new Date("2018-03-01T18:10"),
+		units: 16
+	},
+	{
+		source: "TV",
+		timestamp: new Date("2018-04-13T19:20"),
+		units: 05
+	},
+	{
+		source: "Hairdryer",
+		timestamp: new Date("2018-03-09T07:45"),
+		units: 03
+	},
+	{
+		source: "Tumble Dryer",
+		timestamp: new Date("2018-06-21T13:50"),
+		units: 10
+	},
+	{
+		source: "Kitchen Lights",
+		timestamp: new Date("2018-05-01T22:30"),
+		units: 04
+	},
+	{
 		source: "Hammer Drill",
-		timestamp: new Date("2018-04-12T10:29"),
-		units: 11
+		timestamp: new Date("2018-02-15T12:29"),
+		units: 08
 	},
 	{
 		source: "Air Conditioning",
-		timestamp: new Date("2018-05-16T13:32"),
-		units: 09
+		timestamp: new Date("2018-08-08T11:30"),
+		units: 19
 	},
 	{
 		source: "Washing Machine",
-		timestamp: new Date("2018-05-12T17:00"),
-		units: 21
-	},
-	{
-		source: "Heating",
-		timestamp: new Date("2018-06-22T21:10"),
-		units: 19
-	},
-	{
-		source: "TV",
-		timestamp: new Date("2018-06-08T20:09"),
-		units: 18
-	},
-	{
-		source: "Hairdryer",
-		timestamp: new Date("2018-05-09T07:18"),
-		units: 14
-	},
-	{
-		source: "Tumble Dryer",
-		timestamp: new Date("2018-07-30T11:47"),
+		timestamp: new Date("2018-01-04T12:00"),
 		units: 13
 	},
 	{
+		source: "Heating",
+		timestamp: new Date("2018-02-23T08:10"),
+		units: 26
+	},
+	{
+		source: "TV",
+		timestamp: new Date("2018-05-08T18:32"),
+		units: 02
+	},
+	{
+		source: "Hairdryer",
+		timestamp: new Date("2018-04-11T06:45"),
+		units: 07
+	},
+	{
+		source: "Tumble Dryer",
+		timestamp: new Date("2018-07-10T10:37"),
+		units: 18
+	},
+	{
 		source: "Kitchen Lights",
-		timestamp: new Date("2018-07-01T23:11"),
-		units: 12
+		timestamp: new Date("2018-07-08T06:11"),
+		units: 05
 	},
 	{
 		source: "Hammer Drill",
 		timestamp: new Date("2018-04-12T10:29"),
-		units: 11
+		units: 02
 	},
 	{
 		source: "Air Conditioning",
 		timestamp: new Date("2018-05-16T13:32"),
-		units: 09
+		units: 04
 	},
 
 
